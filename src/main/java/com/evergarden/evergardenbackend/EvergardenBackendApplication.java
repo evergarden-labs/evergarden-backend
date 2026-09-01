f3655cfc0b20973f6c91fe940a5ff536300ff355
soheeGit <63wlsthgml@gmail.com>
2026-09-01 17:47:58 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/evergarden/evergardenbackend/EvergardenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evergarden.evergardenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvergardenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvergardenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evergarden/evergardenbackend/EvergardenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evergarden.evergardenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvergardenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
