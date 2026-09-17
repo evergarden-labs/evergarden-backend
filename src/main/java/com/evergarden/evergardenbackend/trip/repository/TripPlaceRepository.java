0bc6d75c48c2c28d2f4821d8373b1ea92f046c93
soheeGit <63wlsthgml@gmail.com>
2026-09-17 15:05:21 +0900

    feat: findByTripOrderByDayNumberAscSortOrderAsc(Trip trip), countByTrip(Trip trip) 두 개 추가

--- src/main/java/com/evergarden/evergardenbackend/trip/repository/TripPlaceRepository.java
+++ src/main/java/com/evergarden/evergardenbackend/trip/repository/TripPlaceRepository.java
@@ -1,8 +1,13 @@
 package com.evergarden.evergardenbackend.trip.repository;
 
+import com.evergarden.evergardenbackend.trip.entity.Trip;
 import com.evergarden.evergardenbackend.trip.entity.TripPlace;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripPlaceRepository extends JpaRepository<TripPlace, Long> {
+
+    List<TripPlace> findByTripOrderByDayNumberAscSortOrderAsc(Trip trip);
+
+    long countByTrip(Trip trip);
 }
