45adcb9791b270ea0c0c88962cc8c6dbe516ab5c
soheeGit <63wlsthgml@gmail.com>
2026-09-19 02:31:52 +0900

    feat: detailCommon2(공통정보조회) 응답

---
+++ src/main/java/com/evergarden/evergardenbackend/place/client/dto/DetailCommonItem.java
@@ -0,0 +1,5 @@
+package com.evergarden.evergardenbackend.place.client.dto;
+
+/** {@code detailCommon2} 응답 항목. {@code overview}(소개글)만 쓴다. */
+public record DetailCommonItem(String contentid, String overview) {
+}
