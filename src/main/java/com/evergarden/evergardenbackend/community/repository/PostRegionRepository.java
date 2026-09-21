3184ae137196b1b65f0ffa3fe101b4f821c94f54
soheeGit <63wlsthgml@gmail.com>
2026-09-21 23:24:20 +0900

    feat: 저장된 지역 스냅샷을 다시 읽기 위한 쿼리 메서드 1개 추가

--- src/main/java/com/evergarden/evergardenbackend/community/repository/PostRegionRepository.java
+++ src/main/java/com/evergarden/evergardenbackend/community/repository/PostRegionRepository.java
@@ -1,8 +1,12 @@
 package com.evergarden.evergardenbackend.community.repository;
 
+import com.evergarden.evergardenbackend.community.entity.Post;
 import com.evergarden.evergardenbackend.community.entity.PostRegion;
 import com.evergarden.evergardenbackend.community.entity.PostRegionId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRegionRepository extends JpaRepository<PostRegion, PostRegionId> {
+
+    List<PostRegion> findByPost(Post post);
 }
