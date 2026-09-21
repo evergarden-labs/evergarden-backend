2e68ff961784f7f73f9da4e0aa12765149f2f9f9
soheeGit <63wlsthgml@gmail.com>
2026-09-22 00:11:20 +0900

    feat: findByAuthor_IdAndStatusOrderByCreatedAtDesc() 하나 추가

--- src/main/java/com/evergarden/evergardenbackend/community/repository/PostRepository.java
+++ src/main/java/com/evergarden/evergardenbackend/community/repository/PostRepository.java
@@ -1,8 +1,13 @@
 package com.evergarden.evergardenbackend.community.repository;
 
 import com.evergarden.evergardenbackend.community.entity.Post;
-
+import com.evergarden.evergardenbackend.community.entity.PostStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    /** 내가 쓴 게시물을 최신순으로(COMM-09). 삭제한 게시물은 뺀다. */
+    Page<Post> findByAuthor_IdAndStatusOrderByCreatedAtDesc(Long authorId, PostStatus status, Pageable pageable);
 }
